660ebd1dafa47fc1cec2133adb11b2180cbb0e48
Ângelo Rodrigo Sousa Bastos <82399046+relito17@users.noreply.github.com>
2025-04-09 18:13:11 +0100

    first commit 898

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 import CustomCursor from './components/CustomCursor';
 import Navbar from './components/Navbar';
