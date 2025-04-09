8749b2ce8ecc9c2c53ae7d746fe4cba1bc714884
Ângelo Rodrigo Sousa Bastos <82399046+relito17@users.noreply.github.com>
2025-04-09 18:18:44 +0100

    first commit 898

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,13 @@ import './styles/components/services.css';
 import './styles/components/process.css';
 import './styles/components/contact.css';
 
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  window.history.replaceState(null, '', redirect);
+}
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />   
